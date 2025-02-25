ec6f671837e35fdc0b569902c70afc8172df34a3
Skyzo3001 <epepbuuyah1a@gmail.com>
2025-02-25 14:45:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
+global.owner = ['6285603256172']  
+global.mods = ['6285603256172'] 
+global.prems = ['6285603256172']
+global.nameowner = '~*_Skyzo_*~'
+global.numberowner = '6285603256172' 
+global.mail = '*_epepbooyah1a@gmail.com_*' 
 global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
 global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.wm = '© Skyzo'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -17,17 +17,17 @@ global.maxwarn = '5' // Peringatan maksimum Warn
 global.autobio = false // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
 global.antiporn = true // Set true/false untuk Auto delete pesan porno (bot harus admin) (default: true)
 global.spam = true // Set true/false untuk anti spam (default: true)
-global.gcspam = false // Set true/false untuk menutup grup ketika spam (default: false)
+global.gcspam = true // Set true/false untuk menutup grup ketika spam (default: false)
     
 
 // APIKEY INI WAJIB DI ISI! //
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'aDoMsT3p' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 
 
 // INI HANYA OPTIONAL SAJA BOLEH DI ISI BOLEH JUGA ENGGA //
-global.lann = 'YOUR_APIKEY_HERE'
+global.lann = 'Btz-ou45Z'
 //Daftar https://api.betabotz.eu.org 
 
 //Gausah diganti atau di ubah
@@ -50,3 +50,4 @@ fs.watchFile(file, () => {
   delete require.cache[file]
   require(file)
 })
+    
